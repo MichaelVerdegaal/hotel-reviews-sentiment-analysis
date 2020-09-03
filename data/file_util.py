7eb86100c6ee66f00b91967eed6a2b058812c7b9
Michael Verdegaal <michaelverdegaal@hotmail.nl>
2020-09-03 15:17:34 +0200

    clean up main

--- data/file_util.py
+++ data/file_util.py
@@ -7,3 +7,7 @@ manual_csv = ""
 
 def csv_to_df(filepath):
     return pd.DataFrame(pd.read_csv(filepath))
+
+
+def read_kaggle():
+    return csv_to_df(kaggle_csv)
--- main.py
+++ main.py
@@ -1,9 +1,2 @@
-import pandas as pd
+from data.file_util import kaggle_csv
 
-df = pd.DataFrame(pd.read_csv("static/kaggle_reviews.csv"))
-df = df.sort_values(by=['Review_Date', 'Average_Score'])
-
-print(df.head())
-print(df.info())
-print(df.head())
-print(df.head())
