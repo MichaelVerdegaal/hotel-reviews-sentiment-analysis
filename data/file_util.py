4b1749107d107ab550924e3336e54c4a5d92b539
Michael Verdegaal <michaelverdegaal@hotmail.nl>
2020-09-03 15:30:14 +0200

    add dask

--- data/file_util.py
+++ data/file_util.py
@@ -1,13 +1,23 @@
-import pandas as pd
+import dask.dataframe as dd
 
-kaggle_csv = "../static/kaggle_reviews.csv"
+kaggle_csv = "static/kaggle_reviews.csv"
 # TODO: vul path in wanneer het bestand bestaat
 manual_csv = ""
 
 
 def csv_to_df(filepath):
-    return pd.DataFrame(pd.read_csv(filepath))
+    """
+    Reads a csv and converts it to a dataframe
+    :param filepath: path to csv file
+    :return: pandas dataframe
+    """
+    return dd.read_csv(filepath)
 
 
-def read_kaggle():
-    return csv_to_df(kaggle_csv)
+def read_all_csv():
+    """
+    Master function to read all csv files
+    :return: pandas dataframes
+    """
+    kaggle_df = csv_to_df(kaggle_csv)
+    return kaggle_df
--- main.py
+++ main.py
@@ -1,2 +1,3 @@
-from data.file_util import kaggle_csv
+from data.file_util import read_all_csv
 
+print(read_all_csv().head())
