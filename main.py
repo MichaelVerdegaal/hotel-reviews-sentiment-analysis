704156e3fcd97006aab72a32ee26b01107d932f8
Michael Verdegaal <michaelverdegaal@hotmail.nl>
2020-09-09 20:46:36 +0200

    removee unused files

--- main.py
+++ main.py
@@ -5,13 +5,11 @@ from data.scraping import *
 import pandas as pd
 
 c = gather_reviews()
-print(c)
 
 df = pd.DataFrame(c,
                   columns=["Hotel address", "Average Score", "Hotel name", "Nationality", "Negative review",
                            "Positive review", "Score"])
 df.transpose()
-df.to_pickle("./dfpickle.pkl")
 
 print(df.head)
 print(df.info())
