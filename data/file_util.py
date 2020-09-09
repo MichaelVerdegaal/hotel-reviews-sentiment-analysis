5e8e6b74b65b41474336aa0e5f87f2b57f456bd3
Michael Verdegaal <michaelverdegaal@hotmail.nl>
2020-09-09 21:06:24 +0200

    Move functions to right files

--- data/file_util.py
+++ data/file_util.py
@@ -1,8 +1,10 @@
+import os
+import pickle
+
 import dask.dataframe as dd
 
 kaggle_csv = "static/kaggle_reviews.csv"
-# TODO: vul path in wanneer het bestand bestaat
-manual_csv = ""
+manual_csv = "static/manual_reviews.csv"
 
 
 def csv_to_df(filepath):
@@ -21,3 +23,39 @@ def read_all_csv():
     """
     kaggle_df = csv_to_df(kaggle_csv)
     return kaggle_df
+
+
+def write_pickled_txt(object_to_dump, filepath):
+    """
+    Pickled and writes an object to a file as long as the file doesn't exist
+    :param object_to_dump: object that will be written
+    :param filepath: where the file is
+    """
+    if file_exists(filepath):
+        pass
+    else:
+        with open(filepath, 'wb') as f:
+            pickle.dump(object_to_dump, f)
+
+
+def read_pickled_txt(filepath):
+    """
+    Reads from a file and unpickles the stored object
+    :param filepath: where the file is
+    :return: unpickled object
+    """
+    with open(filepath, 'rb') as f:
+        try:
+            unpickled_object = pickle.load(f)
+            return unpickled_object
+        except pickle.UnpicklingError as e:
+            raise e
+
+
+def file_exists(filepath):
+    """
+    Check if a file exists
+    :param filepath: filepath as string
+    :return: boolean
+    """
+    return os.path.isfile(filepath)
--- data/scraping.py
+++ data/scraping.py
@@ -1,5 +1,3 @@
-import os
-import pickle
 import time
 
 from bs4 import BeautifulSoup
@@ -8,44 +6,10 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 # Searching for hotels on booking.com in London that have an average score
+from data.file_util import write_pickled_txt, read_pickled_txt, file_exists
+
 base_url = 'https://www.booking.com'
 hotel_catalog_url = 'https://www.booking.com/searchresults.html?aid=304142&label=gen173nr-1FCAEoggI46AdIM1gEaKkBiAEBmAExuAEKyAEF2AEB6AEB-AECiAIBqAIDuAL_4sP6BcACAdICJGFhY2JlMjA5LWE3ZWMtNGZhZC05MzI2LWM3NmRlMmQ2MGZiZNgCBeACAQ&sid=b7ae3ce18748b2e76e194075e2e4ae6c&tmpl=searchresults&ac_click_type=b&ac_position=1&class_interval=1&dest_id=222&dest_type=country&dtdisc=0&from_sf=1&group_adults=2&group_children=0&inac=0&index_postcard=0&label_click=undef&nflt=ht_id%3D204%3Bclass%3D5%3Bclass%3D4%3Bclass%3D3%3Bclass%3D2%3Bclass%3D1%3Buf%3D-2601889%3B&no_rooms=1&order=class_and_price&postcard=0&raw_dest_type=country&room1=A%2CA&sb_price_type=total&search_selected=1&shw_aparth=0&slp_r_match=0&srpvid=7a84606916b6014e&ss=United%20Kingdom&ss_all=0&ss_raw=uni&ssb=empty&sshis=0&ssne=Spain&ssne_untouched=Spain&top_ufis=1&rows=25'
-
-
-def file_exists(filepath):
-    """
-    Check if a file exists
-    :param filepath: filepath as string
-    :return: boolean
-    """
-    return os.path.isfile(filepath)
-
-
-def write_pickled_txt(object_to_dump, filepath):
-    """
-    Pickled and writes an object to a file as long as the file doesn't exist
-    :param object_to_dump: object that will be written
-    :param filepath: where the file is
-    """
-    if file_exists(filepath):
-        pass
-    else:
-        with open(filepath, 'wb') as f:
-            pickle.dump(object_to_dump, f)
-
-
-def read_pickled_txt(filepath):
-    """
-    Reads from a file and unpickles the stored object
-    :param filepath: where the file is
-    :return: unpickled object
-    """
-    with open(filepath, 'rb') as f:
-        try:
-            unpickled_object = pickle.load(f)
-            return unpickled_object
-        except pickle.UnpicklingError as e:
-            raise e
 
 
 def get_html(page=hotel_catalog_url, headless_mode=True):
